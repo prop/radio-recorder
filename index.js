74bb10f8ecff8016f1dcc6cb8caec32c780abd3f
prop <propoff@gmail.com>
2013-05-13 08:09:08 +0400

    Schedule recordings v0.1.0

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+var dateFormat = require('dateformat');
+var CronJob = require('cron').CronJob;
+var fs = require('fs');
+
+var opts = require('optimist').
+  usage('Usage: $0 [options]').
+  alias('o', 'output').
+  describe('o', 'Output file').
+  alias('u', 'url').
+  describe('u', 'Stream URL').
+  alias('l', 'duration').
+  describe('l', 'Duration of the record in minutes').
+  default('l', 120).
+  alias('d', 'directory').
+  describe('d', 'Output directory').
+  boolean('D').
+  alias('D', 'daemon').
+  describe('D', 'Run in daemon mode').
+  default('d', process.cwd()).
+  alias('c', 'config').
+  describe('c', 'Duration of the record in minutes');
+
+
+var argv = opts.argv;
+
+if (argv.D) {
+  try {
+    var config = parseConfig(argv.c);
+    config.forEach(function(item){
+      var job = new CronJob(item.when, function(){
+        record(argv.d + '/' + item.name + '_' +
+               dateFormat(new Date(), 'isoDateTime') + '.mp3',
+               item.url,
+               item.duration);
+      });
+      
+      job.start();
+    });
+  } catch (e) {
+    console.error('Unable to parse config file', e);
+  }
+} else {
+  var fn = argv.o || argv.d + '/record-' +
+    dateFormat(new Date(), 'isoDateTime') + '.mp3';
+  record(fn, argv.u, argv.l);
+}
+
+
+function record(filename, url, duration) {
+  var spawn = require('child_process').spawn,
+  player  = spawn('mplayer',
+                  ['-playlist', url, '-dumpfile', filename, '-dumpaudio']);
+
+  player.on('close', function (code, signal) {
+    console.log('child process terminated due to receipt of signal ',
+                signal, 'code', code);
+  });
+  
+  setTimeout(function(){
+    player.kill('SIGTERM');
+  }, duration * 60 * 1000);
+}
+
+
+function parseConfig(fn) {
+  var file = fs.readFileSync(fn, 'utf8');
+  return JSON.parse(file); 
+}
+
+exports.record = record;
